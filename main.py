dcd824876832164718958bcf09e2f37944a84fac
Tracie Nguyen <91838259+Tram-ng@users.noreply.github.com>
2022-02-25 09:01:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+random_number =  random.randint(0,1)
+if (random_number == 0):
+   print ("'''\n")
+   print ("Heads\n")
+   print ("'''\n")
+else:
+  print ("'''\n")
+  print ("Tails\n")
+  print ("'''\n")
+
+
+
+
+
+
+
+
